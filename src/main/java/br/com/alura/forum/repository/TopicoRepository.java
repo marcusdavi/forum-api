01dd228284ff70ed12c56e5a1f24334040f21d7d
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2020-10-06 17:40:30 -0300

    Inclusão do projeto desenvolvido no curso de Spring Boot do Alura - Parte 1 e 2

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,17 @@
+package br.com.alura.forum.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.model.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
+	
+//	OutraOpção - Para criar métodos com nome mais amigável
+//	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+//	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+
+}
